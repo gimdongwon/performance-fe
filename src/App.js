91391545c57355f314287ef9f78c25cb9aa7fae9
gimdongwon <ehddnjs8989@gmail.com>
2024-09-07 00:51:54 +0900

    complete 1 lecture code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import './App.css'
-import ListPage from './pages/ListPage/index'
-import ViewPage from './pages/ViewPage/index'
+import React, { Suspense, lazy } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import './App.css';
+// import ListPage from './pages/ListPage/index'
+// import ViewPage from './pages/ViewPage/index'
+
+const ListPage = lazy(() => import('./pages/ListPage'));
+const ViewPage = lazy(() => import('./pages/ViewPage'));
 
 function App() {
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/" component={ListPage} exact />
-        <Route path="/view/:id" component={ViewPage} exact />
-      </Switch>
+    <div className='App'>
+      <Suspense fallback={<div>로딩중..</div>}>
+        <Switch>
+          <Route path='/' component={ListPage} exact />
+          <Route path='/view/:id' component={ViewPage} exact />
+        </Switch>
+      </Suspense>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
